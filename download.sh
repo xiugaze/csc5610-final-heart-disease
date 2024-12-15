1b44fc51bd0575ca9b89f4a94d4b8608d1cd0102
Caleb Andreano <calebandreano@gmail.com>
2024-12-15 04:16:34 +0000

    download script

---
+++ download.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+download() {
+    URL=$1
+    OUTDIR=$2
+
+    mkdir -p "$OUTDIR"
+
+    ZIP_FILE="./$(basename "$URL").zip"
+
+    curl -L -o "$ZIP_FILE" "$URL"
+    unzip -o "$ZIP_FILE" -d "$OUTDIR"
+    rm "$ZIP_FILE"
+}
+
+download "https://www.kaggle.com/api/v1/datasets/download/andrewmvd/heart-failure-clinical-data" "./data/Heart_Failure_Prediction"
+download "https://www.kaggle.com/api/v1/datasets/download/rishidamarla/heart-disease-prediction" "./data/Heart_Disease_Prediction"
+download "https://www.kaggle.com/api/v1/datasets/download/kamilpytlak/personal-key-indicators-of-heart-disease" "./data/Heart_Disease_Dataset"
